51bf85bfc58bbcc33a319a99bf524a132c52cb2e
d4tk1dmVn <datkidmun@gmail.com>
2025-01-25 15:42:05 -0300

    Change way the grid is made, to allow different sizes

--- javascript.js
+++ javascript.js
@@ -16,13 +16,35 @@ function createSquare() {
   return square;
 }
 
-function getGrid() {
-  const grid = document.querySelector("#container");
-  for (let i = 0; i < 16; i++) {
+function createRow(dimension) {
+  const row = document.createElement("div");
+  row.classList.add("row");
+  for (let i = 0; i < dimension; i++) {
     const square = createSquare();
-    grid.appendChild(square);
+    row.appendChild(square);
+  }
+  return row;
+}
+
+function getDimension(amount) {
+  const squares = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100];
+  let dimension = 1;
+  for (let i = 0; i < 10; i++) {
+    if (squares[i] <= amount) {
+      dimension = i + 1;
+    }
+  }
+  return dimension;
+}
+
+function getGrid(amount) {
+  const dimension = getDimension(amount);
+  const grid = document.querySelector("#container");
+  for (let i = 0; i < dimension; i++) {
+    const row = createRow(dimension);
+    grid.appendChild(row);
   }
   return grid;
 }
 
-const grid = getGrid();
+const grid = getGrid(100);
